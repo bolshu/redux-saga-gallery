e0e75f192be4ae2a08fff55cf8799d964266bd82
bolshu <bolshu@MacBook-Pro-bolshu.local>
2019-11-18 23:05:55 +0300

    reducers, actions, store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
-function App() {
-  return (
-    <>
-      <header>
-        <h1>
-          Redux-saga gallery
-        </h1>
-      </header>
-    </>
-  );
+const store = configureStore();
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <>
+          <header>
+            <h1>
+              Redux-saga gallery
+            </h1>
+          </header>
+        </>
+      </Provider>
+    );
+  }
 }
 
 export default App;
