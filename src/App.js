06fe1b9860976c130bcdacebd4bc2f9a539482e6
bolshu <bolshu@MacBook-Pro-bolshu.local>
2020-02-23 12:49:34 +0300

    sagas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
+
+import Header from './components/Header';
+import ImageGrid from './components/ImageGrid';
+
 import configureStore from './store';
 
 const store = configureStore();
-
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <>
-          <header>
-            <h1>
-              Redux-saga gallery
-            </h1>
-          </header>
-        </>
+        <Fragment>
+          <Header />
+          <ImageGrid />
+        </Fragment>
       </Provider>
     );
   }
